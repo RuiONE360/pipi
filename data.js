39e801872b7ed17be00644a4d6779b0f33b7667f
RuiONE360 <80823415+RuiONE360@users.noreply.github.com>
2025-08-29 13:55:43 +0100

    Update data.js

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 module.exports = {
-  token: "YOUR_TELEGRAM_BOT_TOKEN",
-  id:"Enter Your Id",
-  address:"Enter your url"
+  token: "8025031565:AAGMGZ3szCx9hhYhcsItX_y6U7YvCcHR1VE",
+  id:"8025031565",
+  address:"https://pipi-rlai.onrender.com"
 };
